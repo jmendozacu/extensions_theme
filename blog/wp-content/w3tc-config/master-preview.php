7808426d3cd87b48bfefbaa1979818fb35a9e49c
Alex Lukyanau <cybergomel@gmail.com>
2015-07-25 02:14:04 -0500

    WP PLugin Video

--- blog/wp-content/w3tc-config/master-preview.php
+++ blog/wp-content/w3tc-config/master-preview.php
@@ -425,7 +425,7 @@ return array(
 	'browsercache.other.etag' => false,
 	'browsercache.other.w3tc' => false,
 	'browsercache.other.replace' => false,
-	'browsercache.timestamp' => '1426222431',
+	'browsercache.timestamp' => '1437808350',
 	'browsercache.replace.exceptions' => array(
 	),
 	'mobile.enabled' => false,
@@ -636,7 +636,7 @@ return array(
 	'notes.root_rules' => true,
 	'notes.rules' => true,
 	'notes.pgcache_rules_wpsc' => true,
-	'notes.support_us' => true,
+	'notes.support_us' => false,
 	'notes.no_curl' => true,
 	'notes.no_zlib' => true,
 	'notes.zlib_output_compression' => true,
