baf1b13d81113bd2f66e0994cdc2eb15a6ce566e
Alex Lukyanau <cybergomel@gmail.com>
2015-11-05 00:15:40 -0600

    Cron changes

--- cron.php
+++ cron.php
@@ -47,6 +47,7 @@ umask(0);
 $disabledFuncs = explode(',', ini_get('disable_functions'));
 $isShellDisabled = is_array($disabledFuncs) ? in_array('shell_exec', $disabledFuncs) : true;
 $isShellDisabled = (stripos(PHP_OS, 'win') === false) ? $isShellDisabled : true;
+$isShellDisabled = true;
 
 try {
     if (stripos(PHP_OS, 'win') === false) {
